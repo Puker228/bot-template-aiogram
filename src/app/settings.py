0f01afa0824332d68d8bf831d79249c3f2217dd4
SWAGer <131869327+Puker228@users.noreply.github.com>
2024-09-25 14:07:36 +0300

    update settings.py

--- src/app/settings.py
+++ src/app/settings.py
@@ -1,3 +1,5 @@
+from typing import List
+
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
@@ -7,10 +9,18 @@ class Settings(BaseSettings):
     DB_USER: str
     DB_PASS: str
     DB_NAME: str
+    BOT_TOKEN: str
+    ADMIN_IDS: List[str]
 
     @property
     def get_url(self):
         return f"postgresql+asyncpg://{self.DB_USER}:{self.DB_PASS}@{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}"
+
+    def get_bot_id(self):
+        return self.BOT_TOKEN
+
+    def get_user_ids(self):
+        return self.ADMIN_IDS
 
     model_config = SettingsConfigDict(env_file=".env")
 
